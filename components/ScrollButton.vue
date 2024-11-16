34a1b370b48444a0d39eebe745cbc32cabbb78f1
TheDogHusky <contact@classydev.fr>
2024-11-16 22:20:12 +0100

    feat: add content to the website

---
+++ components/ScrollButton.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+const props = defineProps<{
+    scrollTarget: string;
+}>();
+</script>
+
+<template>
+    <a :href="'#' + props.scrollTarget" class="scroll-button" role="button"><Icon name="nf-md-arrow_down_circle" /></a>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,97 @@
 <script setup lang="ts">
+useHead({
+    title: 'Adam Billard, full-stack developer'
+});
 
+const projects = [
+    {
+        name: 'Portfolio',
+        description: 'My personal portfolio, made with Nuxt.js and Tailwind CSS.',
+        links: [
+            {
+                name: 'GitHub',
+                url: 'https://github.com/TheDogHusky/portfolio',
+                icon: 'nf-fa-github'
+            },
+            {
+                name: 'Visit',
+                url: 'https://classydev.fr',
+                icon: 'nf-fa-external_link'
+            }
+        ]
+    },
+    {
+        name: "NekoNya",
+        description: "An anime-themed website with a few functionalities such as a public API.",
+        links: [
+            {
+                name: 'GitHub',
+                url: 'https://github.com/NekoNyaDevs',
+                icon: 'nf-fa-github'
+            },
+            {
+                name: 'Visit',
+                url: 'https://nekonya.classydev.fr',
+                icon: 'nf-fa-external_link'
+            }
+        ]
+    },
+    {
+        name: 'Hestia',
+        description: 'A Discord community that I made a website for.',
+        links: [
+            {
+                name: 'Visit',
+                url: 'https://project-hestia.me',
+                icon: 'nf-fa-external_link'
+            }
+        ]
+    },
+    {
+        name: 'nathanpradel.fr',
+        description: 'A website for a friend of mine, to showcase his photographic work.',
+        links: [
+            {
+                name: 'Visit',
+                url: 'https://nathanpradel.fr',
+                icon: 'nf-fa-external_link'
+            }
+        ]
+    }
+];
 </script>
 
 <template>
     <main class="container child-extend px-4">
-        <h1>Hey, I'm <span class="title-special">Adam</span>.</h1>
-        <Socials place="header" />
+        <NuxtImg alt="logo" format="webp" src="/logo.webp" height="720" width="720" class="main-logo" />
+        <div class="main-header">
+            <h1 data-aos="fade-right">Hey, I'm <span class="title-special">Adam</span>.</h1>
+            <Socials data-aos="fade-up" place="header" />
+        </div>
+        <ScrollButton scrollTarget="about" />
     </main>
-    <section class="container">
-
+    <section id="about" class="container bg-darker gap-3 px-4">
+        <h2 data-aos="fade-right">About me</h2>
+        <p class="about-p" data-aos="fade-up">I'm a full-stack 16 years old developer from France. I'm passionate about web development and I love to create beautiful and efficient websites.</p>
+    </section>
+    <section class="container gap-3 p-4" id="projects">
+        <h2 data-aos="fade-right">Projects</h2>
+        <ul class="projects">
+            <li v-for="project in projects" :key="project.name" class="project" data-aos="fade-up">
+                <h3>{{ project.name }}</h3>
+                <p>{{ project.description }}</p>
+                <ul class="project-links">
+                    <li v-for="link in project.links" :key="link.name">
+                        <a :href="link.url" target="_blank" class="btn btn-primary" rel="noopener noreferrer">
+                            <Icon :name="link.icon" />
+                            {{ link.name }}
+                        </a>
+                    </li>
+                </ul>
+            </li>
+        </ul>
+    </section>
+    <section class="container gap-3 p-4 bg-darker" id="contact">
+        <h2 data-aos="fade-right">Contact</h2>
     </section>
 </template>
