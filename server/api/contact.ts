6809c748fa4506d0850699de3a786f50f5b4860b
TheDogHusky <contact@classydev.fr>
2024-11-17 13:59:32 +0100

    feat: add contact api route

--- server/api/contact.ts
+++ server/api/contact.ts
@@ -1,5 +1,6 @@
 import { createTransport } from 'nodemailer';
 import { z } from 'zod';
+import xss from 'xss';
 
 const config = useRuntimeConfig();
 
@@ -13,17 +14,41 @@ const transporter = createTransport({
     }
 });
 
-export default defineEventHandler((event) => {
+const ContactSchema = z.object({
+    name: z.string({ message: 'The name field must be a string' }).min(2, 'The name field must contain at least 2 characters').max(50, 'The name field must contain at most 50 characters'),
+    email: z.string({ message: 'The email field must be a string' }).trim().email('The email field must be a valid email address'),
+    message: z.string({ message: 'The message field must be a string' }).min(50, 'The message field must contain at least 50 characters').max(1500, 'The message field must contain at most 1500 characters')
+});
+
+export default defineEventHandler(async (event) => {
     if (event.method !== 'POST') {
-        return {
-            status: 405,
-            body: {
-                message: 'Method not allowed'
-            }
-        }
+        throw createError({
+            statusText: 'Method Not Allowed',
+            statusCode: 405
+        });
     }
 
+    const result = await readValidatedBody(event, body => ContactSchema.safeParse(body));
+    if (!result.success) throw createError({
+        statusText: result.error.issues.map(issue => issue.message).join(', '),
+        statusCode: 400
+    });
+
+    const { name, email, message } = result.data;
+    const sanitizedMessage = xss(message);
+
+    await transporter.sendMail({
+        from: {
+            name: email,
+            address: config.mailUser
+        },
+        to: 'contact@classydev.fr',
+        subject: `[Contact Form] New message from ${name}`,
+        text: sanitizedMessage,
+        replyTo: email
+    });
+
     return {
-        hello: 'world'
-    }
-})
+        statusCode: 204
+    };
+});
