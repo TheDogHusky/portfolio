bb65ff20ffc1debbb6e760083b77883442fde960
TheDogHusky <contact@classydev.fr>
2024-11-20 17:03:06 +0100

    feat: add a footer

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -1,11 +1,11 @@
-<script setup lang="ts">
-
-</script>
-
 <template>
-
+    <footer data-aos="fade-up" data-aos-anchor="#contact" data-aos-anchor-placement="top-center">
+        <div>
+            <p>&copy; 2024 Adam Billard, all rights reserved.</p>
+            <p>Website made with ❤️ by Adam Billard.</p>
+        </div>
+        <Socials place="footer" />
+    </footer>
 </template>
-
-<style scoped>
-
-</style>
+<script setup lang="ts">
+</script>
